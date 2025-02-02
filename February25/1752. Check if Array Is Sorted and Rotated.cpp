5f98d6aaf05f0db0d68ae01f206678d891500ee2
Ayush Sharma <141551003+ayushh201@users.noreply.github.com>
2025-02-02 09:16:54 +0530

    Update 1752. Check if Array Is Sorted and Rotated.cpp

--- February25/1752. Check if Array Is Sorted and Rotated.cpp
+++ February25/1752. Check if Array Is Sorted and Rotated.cpp
@@ -19,3 +19,16 @@ public:
         return false;
     }
 };
+
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n=nums.size();
+        int peak=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>nums[(i+1)%n]) peak++;
+        }
+        return peak<=1;
+    }
+};
