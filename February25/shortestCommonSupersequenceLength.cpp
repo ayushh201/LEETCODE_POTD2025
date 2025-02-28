47ae422135be080ace54a063e004bdbdbc6901aa
Ayush Sharma <141551003+ayushh201@users.noreply.github.com>
2025-02-28 13:32:04 +0530

    Create shortestCommonSupersequenceLength.cpp

---
+++ February25/shortestCommonSupersequenceLength.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to find length of shortest common supersequence of two strings.
+    int shortestCommonSupersequence(string &s1, string &s2) {
+        int m=s1.length();
+        int n=s2.length();
+        vector<vector<int>> t(m+1,vector<int>(n+1,0));
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0){
+                    t[i][j]=i+j;
+                }else if(s1[i-1]==s2[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                }else{
+                    t[i][j]=1+min(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return t[m][n];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    // taking total testcases
+    cin >> t;
+    while (t--) {
+        string X, Y;
+        // taking String X and Y
+        cin >> X >> Y;
+
+        // calling function shortestCommonSupersequence()
+        Solution obj;
+        cout << obj.shortestCommonSupersequence(X, Y) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
